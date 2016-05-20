9947cd45fc01c8a02437101d91255261581d998c
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2016-05-20 15:31:19 -0700

    Trying to create a mean average percision algorithm

--- data_read.py
+++ data_read.py
@@ -3,9 +3,10 @@ import numpy as np
 import matplotlib as plt
 
 
-def readIn():
-	train = pd.read_csv("../train.csv")
-	test = pd.read_csv("../test.csv")
+def displayTop(train, test):
+	'''
+		Shows the top and sample statistics of the train and test datasets
+	'''
 	
 	print(train.describe())
 	print(test.describe())
@@ -14,7 +15,20 @@ def readIn():
 	print(test.head())
 	
 	return(train)
+	
+def meanPreciscion(output):
+	'''
+		Calculates the Mean Average Percision @ 3
+	'''
+	#for i in range(0, len(output.index)):
+	output[.str.split(
+	
 
 if __name__ == '__main__':
-	readIn()
+
+	train = pd.read_csv("../train.csv")
+	test = pd.read_csv("../test.csv")
+	sample = read_csv("../sample_submission.csv")
+	displayTop(train, test)
+	meanPrecision(sample)
 	
