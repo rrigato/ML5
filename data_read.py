ee16f775667a60ba4f652bfb18bbff545e16e4f1
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2016-05-26 15:49:28 -0700

    Trying to implement a knn algorithm

--- data_read.py
+++ data_read.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib as plt
-
+from sklearn import preprocessing, cross_validation, neighbors
 
 def displayTop(train, test):
 	'''
@@ -38,15 +38,51 @@ def meanPrecision(output):
 	print(np.mean(average_precisions))
 	
 
+
+def knnImplement(train, test):
+	ids = train['row_id']
+	train.drop(['row_id'], 1)
+	X = np.array(train.drop(['place_id'],1))
+	y = np.array(train['place_id'])
+	
+	X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=.2)
+	
+	clf = neighbors.KNeighborsClassifier()
+	clf.fit(X_train, y_train)
+	accuracy = clf.score(X_test, y_test)
+	print(accuracy)
+	return()
+	
 	
 
 if __name__ == '__main__':
 
-	#train = pd.read_csv("../train.csv")
+	train = pd.read_csv("../train.csv")
 	#test = pd.read_csv("../test.csv")
 	#sample = pd.read_csv("../sample_submission.csv")
 	
-	row_id = [0, 1]
+	print(len(train))
+
+	train2 = train.ix[0:100,:]
+	
+	test2 = train.ix[100:200,:]
+#	train2= train.ix[0:(len(train)/2), :]
+#	test2 = train.ix[(len(train)/2):(len(train)-1),:]
+	
+	group_by_response = train.groupby('place_id')
+	
+	group_by_response.head()
+	
+	#rows = 29118021
+	#unique check ins = 108390
+
+	print(len(np.unique(train['place_id'])) )
+	knnImplement(train2,test2)
+
+
+
+
+'''	row_id = [0, 1]
 	check_in = ['000 001 002','003 004 005']
 
 	actual = ['000', '004']
@@ -59,4 +95,4 @@ if __name__ == '__main__':
 
 	#displayTop(train, test)
 	meanPrecision(sample)
-	
+'''	
