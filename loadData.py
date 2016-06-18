b9a0523d7ec6d709dac6eb913ab71cc4e0ee02a4
rrigato <rrigato@horizon.csueastbay.edu>
2016-06-18 08:32:32 -0400

    Trying to see if the train dataset will fit into ram

--- loadData.py
+++ loadData.py
@@ -7,16 +7,29 @@ import sys
 class cluster():
 	def __init__(self):
 		print("Initializing the data....")
+		self.readData("write")
+
 		
 	def readData(self, option):
-		try:
-		except IOError as e:
-			print("The data has not been serialized I will do that now")
-			
-		except:
-			print("Unknown Error: ")
-			print(sys.exe[0])
+		if option == 'write':
+			try:
+				file = open("C:/Users/Punkiehome1/Downloads/kaggleFacebook/train.csv","r")
+			except IOError as e:
+				print("The data has not been serialized I will do that now")
+				self.train = pd.read_csv("C:/Users/Punkiehome1/Downloads/kaggleFacebook/train.csv")
+				#pickle.dump(self.train, open("C:/Users/Punkiehome1/Downloads/kaggleFacebook/train.p",
+				#	"wb"))
+			except:
+				print("Unknown Error: ")
+				print(sys.exc_info()[0])
+				sys.exit(1)
+			else:
+				file.close()
+		elif option == 'read':
+			print("hi")
+			# try:
+			# except:
 			
 
 if __name__ == "__main__":
-	
+	myCls = cluster()
