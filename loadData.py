e617968ca17f3fe08d6bdd7dd912c6287c8eb369
rrigato <rrigato@horizon.csueastbay.edu>
2016-06-17 21:39:04 -0400

    Adding a class that can serialize the data

---
+++ loadData.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import sys
+
+
+class cluster():
+	def __init__(self):
+		print("Initializing the data....")
+		
+	def readData(self, option):
+		try:
+		except IOError as e:
+			print("The data has not been serialized I will do that now")
+			
+		except:
+			print("Unknown Error: ")
+			print(sys.exe[0])
+			
+
+if __name__ == "__main__":
+	
