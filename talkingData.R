61df45894314925304e073ec5a758d7534dd65b9
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2016-07-23 14:19:36 -0400

    Dealing with 1 to many join, converting timestamp to numeric

--- talkingData.R
+++ talkingData.R
@@ -15,25 +15,22 @@ brand = as.data.frame(fread("C:\\Users\\ryan\\kaggleTD\\phone_brand_device_model
 
 categories = as.data.frame(fread("C:\\Users\\ryan\\kaggleTD\\label_categories.csv", sep = ","))
 
-train2 = sqldf("SELECT a.device_id, b.event_id, b.timestamp, b.longitude,
-	b.latitude FROM train as a LEFT JOIN events as b on a.device_id = b.device_id")
+train$device_id = as.numeric(train$device_id)
+brand$device_id = as.numeric(brand$device_id)
+test$device_id = as.numeric(test$device_id)
+events$device_id = as.numeric(events$device_id)
+#group is a keyword in sql, change the y from group to response
+names(train)[4] = "response"
+
+events$timestamp = as.integer(as.POSIXct(events$timestamp))
 
 
 
+train2 = sqldf("SELECT a.device_id, a.response, a.gender, a.age,   b.phone_brand, b.device_model
+FROM train AS a LEFT JOIN brand AS b on a.device_id = b.device_id")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+test2 = sqldf("SELECT a.device_id, b.event_id, SUM(b.timestamp)
+FROM test AS a LEFT JOIN events AS b on a.device_id = b.device_id
+GROUP BY 1")
 
